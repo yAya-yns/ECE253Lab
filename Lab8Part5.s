83058f7d4d92708c7a7ad9d2524c8841ba5b0148
Yannis <33261653+yAya-yns@users.noreply.github.com>
2019-11-26 22:33:49 -0500

    update Lab8Part5
    Completed but having a bug
    
    when a zero is passed in, the 1's counter, r8 became zero, even when '''movlt r8,r5''' is used

--- Lab8Part5.s
+++ Lab8Part5.s
@@ -3,40 +3,81 @@
 	
 _start:
 	//initialization
-	//register used: r0(address of current word), r1 (storing value for current word), r2(temp) 
+	//register used: r0(address of current word), r1 (storing value for current word), r2(temp variable to see the result of masking) 
 	//r5-r7 (counter for current word's ones, leading, trailing)
 	//r8-r10 (counter for greatest ones, leading, trailing)
 	mov r8,#0
 	mov r9,#0
 	mov r10,#0
 	ldr r0,=LIST
-	ldr r1,[r0]
+	
 	
 mainLoop:
+	ldr r1,[r0]
 	cmp r1,#-1
 	beq END
-	
-ONES:
+	//counting ones	
 	mov r5,#0//this value is greater than the current "max"
-	mov r3,#0
-	ldr r1,[r0]//restore the value for the next process (Leading)
+	mov r2,#0//temp variable to see the result of masking
+ONESLoop:	
 	cmp r1,#0//see if this number don't have more 1's
 	beq storeOnes
-	and r3,r1,#1
-	add r5,r3
+	and r2,r1,#1
+	add r5,r2
+	lsr r1,#1
+	b ONESLoop
 storeOnes:
-	movle r8,r5
+	movlt r8,r5
+
 	
-	
-	
-	
-	
+	//counting leading zeros
+	mov r6,#0//this value is greater than the current "max"
+	mov r2,#0//temp variable to see the result of masking
+	ldr r1,[r0]//making sure the value is before any modification
+	cmp r1,#0
+	beq LdZero
+LeadingZerosLoop:
+	and r2,r1,#2147483648
+	cmp r2,#2147483648
+	beq storeLdZeros
+	add r6,#1
+	lsl r1,#1
+	b LeadingZerosLoop
+storeLdZeros:
+	movle r9,r6
+	b JumpLdZero	
+LdZero:
+	mov r9,#32
+JumpLdZero:
+	//counting trailing zeros
+	mov r7,#0//this value is greater than the current "max"
+	mov r2,#0//temp variable to see the result of masking
+	ldr r1,[r0]//making sure the value is before any modification
+	cmp r1,#0//if the value passed in is 0
+	beq TlZero
+TrailingZerosLoop:
+	and r2,r1,#1
+	cmp r2,#1
+	beq storeTlZeros
+	add r7,#1
+	lsr r1,#1
+	cmp r1,#0
+	beq storeTlZeros
+	b TrailingZerosLoop
+storeTlZeros:
+	movle r10,r7
+	b JumpTlZero	
+TlZero:
+	mov r10,#32
+JumpTlZero:
+	add r0,#4
+	b mainLoop
 	
 	
 END:
-	b end
+	b END
 	
-LIST: .word 0b0011100,0b111,0b11001,0b01100,-1	
+LIST: .word 0b00011111111111111100111111111000,0,-1	
 	
 .end
 
